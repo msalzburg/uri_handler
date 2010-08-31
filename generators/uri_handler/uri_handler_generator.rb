21532fcc93c9275c737786b10106f51ec50cfa06
msalzburg <salzburg@emjot.de>
2010-08-31 14:42:04 +0200

    * finished generator

--- generators/uri_handler/uri_handler_generator.rb
+++ generators/uri_handler/uri_handler_generator.rb
@@ -1,27 +1,29 @@
-class URIHandlerGenerator < Rails::Generator::NamedBase
-  attr_accessor :uri_attributes, :migration_name
+class UriHandlerGenerator < Rails::Generator::NamedBase
+  attr_accessor :uri_columns, :migration_name
 
   def initialize(args, options = {})
     super
-    @class_name, @uri_attributes = args[0], args[1..-1]
+    @class_name, @uri_columns = args[0], args[1..-1]
   end
 
   def manifest
     file_name       = generate_file_name
     @migration_name = file_name.camelize
     record do |m|
-      m.migration_template "paperclip_migration.rb.erb",
-                           File.join('db', 'migrate'),
-                           :migration_file_name => file_name
+      m.migration_template(
+        "uri_handler_migration.rb.erb", 
+        File.join('db', 'migrate'), 
+        :migration_file_name => file_name
+      )
     end
   end
 
   private
 
   def generate_file_name
-    names = uri_attributes.map{|a| a.underscore }
+    names = uri_columns.map{|a| a.underscore }
     names = names[0..-2] + ["and", names[-1]] if names.length > 1
-    "add_uri_attributes_#{names.join("_")}_to_#{@class_name.underscore}"
+    "add_uri_handler_#{names.join("_")}_columns_to_#{@class_name.underscore}"
   end
 
 end
